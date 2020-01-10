59520b0e8481fb0c0ed27c4aca456d369e3c6d22
Tedongmo <wilfried.tedongmo@nanobnk.com>
2020-01-10 17:07:34 +0100

    UPD: Admin Module html View

--- apps/administration/src/main/kotlin/com/afsoltech/config/CreateRubric.kt
+++ apps/administration/src/main/kotlin/com/afsoltech/config/CreateRubric.kt
@@ -1,5 +1,8 @@
 package com.afsoltech.config
 
+import com.afsoltech.core.entity.security.Menu
+import com.afsoltech.core.entity.security.Rubric
+import com.afsoltech.core.model.attribute.BaseStatus
 import com.afsoltech.core.repository.security.MenuRepository
 import com.afsoltech.core.repository.security.RubricRepository
 import mu.KLogging
@@ -8,12 +11,11 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping
 
-//@RequestMapping(value = ["/users", "/"])
-@ConditionalOnProperty(name = ["spring.jpa.hibernate.ddl-auto"], havingValue = "update")
+@ConditionalOnProperty(name = ["spring.jpa.hibernate.ddl-auto"], havingValue = "create")
 @Configuration
-@ComponentScan("com.afsoltech.kops")
 class CreateRubric(val requestMappingHandlerMapping: RequestMappingHandlerMapping, val rubricRepository: RubricRepository,
                    val menuRepository: MenuRepository)  {
 
@@ -22,32 +24,43 @@ class CreateRubric(val requestMappingHandlerMapping: RequestMappingHandlerMappin
     @Bean
     fun getEndPointsInView() = CommandLineRunner {
 
-        val keys = requestMappingHandlerMapping.handlerMethods.keys
+//        val keys = requestMappingHandlerMapping.handlerMethods.keys
         val methods = requestMappingHandlerMapping.handlerMethods
 
-//        val menuOp = menuRepository.findById(1)
-//        var menu : Menu
-//        if(menuOp.isPresent)
-//            menu = menuOp.get()
-//        else menu = Menu(1, "Initial Menu")
-//        menu.status = BaseStatus.ACTIVE
-//        menu = menuRepository.save(menu)
-//
-//        val rubricList = mutableListOf<Rubric>()
-//        var index :Long =1
-//        keys.forEach { key ->
-//            if(key.methodsCondition.methods.contains(RequestMethod.GET)){
-//                val rubric = Rubric(id = index++, code = null, label = key.name?:key.patternsCondition.patterns.elementAt(0),
-//                        uri = key.patternsCondition.patterns.elementAt(0), menu = menu)
-//                rubric.status = BaseStatus.ACTIVE
-//                rubricList.add(rubricRepository.save(rubric))
-//            }
-//        }
+        var menuOp = menuRepository.findById(1)
+        var menu : Menu
+        if(menuOp.isPresent)
+            menu = menuOp.get()
+        else menu = Menu(1, "Security")
+        menu.status = BaseStatus.ACTIVE
+        menu = menuRepository.save(menu)
 
-        logger.trace { "\n Execution of config class" }
-        methods.forEach{ key, value ->
-            logger.trace { "$key ==== ${value.beanType.`package`}" }
+        menuOp = menuRepository.findById(2)
+        var menu2 : Menu
+        if(menuOp.isPresent)
+            menu2 = menuOp.get()
+        else menu2 = Menu(2, "Admin")
+        menu2.status = BaseStatus.ACTIVE
+        menu2 = menuRepository.save(menu2)
+
+        val rubricList = mutableListOf<Rubric>()
+        var index :Long =1
+        methods.forEach { key, value ->
+            logger.trace { "$key ==== ${value.beanType.`package`.name}" }
+            if(key.methodsCondition.methods.contains(RequestMethod.GET) && (value.beanType.`package`.name.contains("administration", true))){
+
+                val boolMenu= value.beanType.`package`.name.contains("security", true)
+                val rubric = Rubric(id = index++, code = null, label = key.name?:key.patternsCondition.patterns.elementAt(0),
+                        uri = key.patternsCondition.patterns.elementAt(0), menu = if(boolMenu) menu else menu2)
+                rubric.status = BaseStatus.ACTIVE
+                rubricList.add(rubricRepository.save(rubric))
+            }
         }
+
+//        logger.trace { "\n Execution of config class" }
+//        methods.forEach{ key, value ->
+//            logger.trace { "$key ==== ${value.beanType.`package`.name}" }
+//        }
 //        requestMappingHandlerMapping.pathPrefixes
 //        val paths = requestMappingHandlerMapping.pathPrefixes
 //        paths.forEach{ key, value ->
